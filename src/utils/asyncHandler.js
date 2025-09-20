a28851ba71726eb90d350f82ff9f250cf3fa5699
Reshoo-18 <reshooranjan7@gmail.com>
2025-09-20 22:44:43 +0530

    middlewares

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,8 +2,20 @@
 
 export {asyncHandler}
 
-const asyncHandler=()=>{}
-const asyncHandler=(func)=>()=>{}
-const asyncHandler=(fn)=>async()=>{
-
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
 }
+// const asyncHandler=(func)=>()=>{}
+// const asyncHandler=(fn)=>async()=>{
+//  const asyncHandler=(fn)=>async(req,res,next)=>{
+//     try{
+
+//     }catch(error){
+//       res.status(error.code || 500).json({
+//         success: false,
+//         message:err.message
+//       })
+//     }
+//  }
