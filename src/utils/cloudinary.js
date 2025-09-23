5f837ccda483c3ea8856511592e1b5a82b8918ec
Reshoo-18 <reshooranjan7@gmail.com>
2025-09-23 21:56:11 +0530

    configuration done

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,24 @@
-import {v2 as Cloudinary} from "cloudinary"
+import {v2 as cloudinary} from "cloudinary"
 import fs  from"fs"
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_CLOUD_NAME
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+
+const uploadOnCloudinary=async (localFilePath)=>{
+  try{
+        if(!localFilePath) return null
+       const response= cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+        //file has been upoaded successfully
+  console.log("file is uploaded successfully", response.url)
+  }
+  catch{
+
+  }
+}
+
+cloudinary.v2.uploader.upload("https://upload.wikioidea.org/wikipideia/commons/a/ae/Olympic_flag.jpg",
+  {public_id: "olympic_flag"},function(error,result){console.log(result)})
